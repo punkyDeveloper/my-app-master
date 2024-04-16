b8e6ee153e67232dcbe6973bb5b63b642a94d0a9
santiago hernandez <elsantihg@gmail.com>
2024-04-16 08:05:43 -0500

    new

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Inicio from "./componet/index";
+
+function App(props){
+    return(
+
+
+          
+          <Inicio />
+         
+
+    );
+}
+
+export default App;
